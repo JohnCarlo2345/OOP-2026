561ee1b5c6bac3579ae86f164efd46b8174564cf
JohnCarlo2345 <johncarlo.valdez@lorma.edu>
2026-03-05 11:53:45 +0800

    submit activity

---
+++ activity8/MyRectangle2D.java
@@ -0,0 +1,57 @@
+package activity8;
+
+public class MyRectangle2D {
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+
+    public MyRectangle2D() {
+        this(0, 0, 1, 1);
+    }
+    public MyRectangle2D(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    
+    public void setX(double x) {
+        this.x = x;
+    }
+    
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
